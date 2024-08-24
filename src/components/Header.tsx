5c4aabc043055ad8e322009b28857d66d6e3d9e2
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2024-08-24 14:11:37 +0200

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-black border-2 border-gray-800 rounded-full p-4 ml-20 mr-20 mt-5 mb-10">
+    <header className="bg-black border-2 border-gray-800 rounded-full p-4 mx-5 mt-5 mb-10 md:mx-20">
       <div className="container mx-auto flex items-center justify-between">
         {/* Logo Section */}
         <div className="flex items-center">
@@ -62,7 +62,7 @@ const Header = () => {
 
       {/* Menu déroulant pour les petits écrans */}
       {isOpen && (
-        <nav className="md:hidden bg-black border-t border-gray-700">
+        <nav className="md:hidden bg-black border-t border-gray-700 flex flex-col items-center">
           <a href="#" className="block text-white px-4 py-2 hover:bg-gray-800">
             Home
           </a>
