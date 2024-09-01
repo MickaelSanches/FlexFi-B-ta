dc84af869fd82a74bfb9cbc43fcabdae1144a7ed
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2024-09-01 17:52:58 +0200

    fix bug

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useLoginStore } from '@/store/useLoginStore';
 
