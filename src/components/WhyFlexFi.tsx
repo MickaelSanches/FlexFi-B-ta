dc9c3fb499cc82e217e8907654ad69b1cf76cd76
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2024-09-25 22:00:02 +0200

    change banner

--- src/components/WhyFlexFi.tsx
+++ src/components/WhyFlexFi.tsx
@@ -55,7 +55,7 @@ export const WhyFlexFi = () => {
               </li>
             </ul>
 
-            <button className="mt-8 px-6 py-3 bg-[#0C8CF3] text-white font-semibold rounded-lg hover:bg-[#85C8FF] transition duration-300">
+            <button className="mt-8 px-6 py-3 bg-gradient-to-r from-[#00FEFB] to-[#60259E] hover:from-[#85C8FF] hover:to-[#0C8CF3] text-white font-bold rounded-lg shadow-lg transform transition duration-300 hover:scale-105">
               Get Started with FlexFi
             </button>
           </div>
