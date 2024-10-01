7574acbdbb7e7c590508782ac73667781257a14c
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2024-10-01 15:42:53 +0200

    [Home] change background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,49 +13,17 @@ export default function Home() {
     <>
       {/* <ComingSoon /> */}
       <main className="w-full">
-        <div className="bg-gray-950 flex flex-col md:flex-row justify-between items-center h-auto pt-16 pb-16 px-4 md:px-32">
+        <div
+          className="bg-gray-950 flex flex-col md:flex-row justify-between items-center h-screen pt-16 pb-16 px-4 md:px-32 bg-cover bg-center"
+          style={{
+            backgroundImage: "url('/images/flexfi-crypto-payment.webp')",
+          }}
+        >
           {/* Presentation component on top for smaller screens */}
           <div className="mb-8 md:mb-0">
             <Presentation />
           </div>
-          <img
-            src="/logo/flexicon.webp"
-            alt="FlexFi icone"
-            className="h-64 w-auto md:h-full neon-effect animate-neon-pulse"
-          />
         </div>
-
-        <style>{`
-          /* Néon effect */
-          .neon-effect {
-            filter: drop-shadow(0 0 8px #00fefb) drop-shadow(0 0 16px #00fefb);
-            animation: slideInRight 1s ease-out forwards,
-              neon 1.5s infinite alternate;
-          }
-
-          /* Pulsation and neon animation */
-          @keyframes neon {
-            from {
-              filter: drop-shadow(0 0 8px #00fefb) drop-shadow(0 0 16px #00fefb);
-            }
-            to {
-              filter: drop-shadow(0 0 14px #00fefb)
-                drop-shadow(0 0 28px #00fefb);
-            }
-          }
-
-          /* Slide in from the right */
-          @keyframes slideInRight {
-            0% {
-              opacity: 0;
-              transform: translateX(100%);
-            }
-            100% {
-              opacity: 1;
-              transform: translateX(0);
-            }
-          }
-        `}</style>
 
         {/* Sections below maintain their own responsive layout */}
 
