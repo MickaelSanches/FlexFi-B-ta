7153174eb68a95ddb2f48ed492170e1321725e61
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2024-10-05 13:14:07 +0200

    creat getPrivatekeyByEmail and payInstallment methode

--- src/repositories/bnplRepository.ts
+++ src/repositories/bnplRepository.ts
@@ -44,9 +44,50 @@ export const bnplRepository = () => {
     }
   };
 
+  const getPrivatekeyByEmail = async (email: string) => {
+    try {
+      const response = await axios.get(`${URL_API}/user/${email}/privateKey`);
+
+      if (response.status === 200) {
+        const privateKey = response.data.privateKey; // Assuming the private key is inside 'data.privateKey'
+        return privateKey; // Return the private key
+      } else {
+        console.error(`Error: Received status ${response.status}`);
+        return null; // Return null if the status is not 200
+      }
+    } catch (error) {
+      console.error("Error fetching private key:", error);
+      return null; // Return null in case of an error
+    }
+  };
+
+  const payInstallment = async (saleId: number, email: string) => {
+    try {
+      const buyerPrivateKey = await getPrivatekeyByEmail(email);
+
+      const payment = await axios.post(`${URL_API}/payment`, {
+        buyerPrivateKey,
+        saleId,
+      });
+
+      if (payment.status === 200) {
+        console.log("Payment successful:", payment.data);
+        return payment.data; // Return the data if payment is successful
+      } else {
+        console.error("Payment error:", payment.status);
+        return null; // Return null if the response status is not 200
+      }
+    } catch (error) {
+      console.error("Error making payment request:", error);
+      return null; // Return null in case of an error
+    }
+  };
+
   return {
     getPurchases,
     downloadPurchaseInfoInPDF,
     getPurchasDetails,
+    payInstallment,
+    getPrivatekeyByEmail,
   };
 };
